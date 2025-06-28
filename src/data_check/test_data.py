ce12e4eb4ef3f4f4367393454fefb357325c0442
py1150 <bernd@Bernds-MacBook-Pro.local>
2025-06-28 11:29:23 +0200

    data_check component integrated

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data: pd.DataFrame):
+    """
+    Test that the number of rows in the dataset is between 15,000 and 1,000,000.
+    """
+    assert 15000 < data.shape[0] <  1000000
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Test that the price is between the specified minimum and maximum value.
+    """
+    assert data['price'].between(min_price, max_price).all(), "Price values are not within the expected range."
